8e8ad79cd6b744f8d9592e1d2ce42d8c4418aaa4
Goga <barbaradob17@gmail.com>
2022-10-21 22:15:58 +0300

    check env var

--- config/__init__.py
+++ config/__init__.py
@@ -8,9 +8,16 @@ load_dotenv()
 
 
 BOT_TOKEN = getenv('BOT_TOKEN')
+ADMIN_ID = getenv('ADMIN_ID')
+GROUP_ID =getenv('GROUP_ID')
 PARSING_LINK = 'https://kbp.by/rasp/timetable/view_beta_kbp/?page=stable&cat=group&id=62'
 
 
 if BOT_TOKEN is None:
     raise SystemExit('Token is not defined')
 
+if ADMIN_ID is None:
+    raise SystemExit('Admin is not defined')
+
+if GROUP_ID is None:
+    raise SystemExit('Group is not defined')
